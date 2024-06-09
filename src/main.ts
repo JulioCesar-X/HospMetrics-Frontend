820129d35c464baffb12f9c0afb523a559cda6a2
Julio Cesar <julio.pereira.t0126213@edu.atec.pt>
2024-06-09 13:52:12 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/env';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ApiInterceptor } from './app/core/interceptors/api.interceptor';
+import { importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+console.log('Environment:', environment);
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiInterceptor,
+      multi: true
+    },
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
